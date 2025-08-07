981b5ad1a2e7999bd1f9787c9fe574ac03eac70b
TArslan7 <T_Arslan7@hotmail.com>
2025-08-07 05:10:31 +0300

    fix: Enable core library desugaring for flutter_local_notifications
    - Added isCoreLibraryDesugaringEnabled = true to compileOptions
    - Added coreLibraryDesugaring dependency for Android build
    - This resolves the AAR metadata issue with flutter_local_notifications

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
 android {
     namespace = "com.example.comnecter_mobile"
     compileSdk = flutter.compileSdkVersion
@@ -13,6 +17,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
